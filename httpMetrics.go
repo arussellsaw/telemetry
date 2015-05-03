4dc7769f336547a62c060cf9333f4c7cc5ed8669
arussellsaw <alexrussellsaw@gmail.com>
2015-05-03 20:08:27 +0100

    totals, counters, and averages

--- httpMetrics.go
+++ httpMetrics.go
@@ -10,10 +10,26 @@ type httpMetrics struct {
 }
 
 func (h httpMetrics) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for key, value := range h.metrics.floatMetrics {
+	for key, value := range h.metrics.points {
 		w.Write([]byte(fmt.Sprintf("%s %v \n", key, value)))
 	}
-	for key, value := range h.metrics.intMetrics {
-		w.Write([]byte(fmt.Sprintf("%s %v \n", key, value)))
+	for key, value := range h.metrics.averages {
+		var sum = float32(0)
+		if len(value.points) > 0 {
+			for i := range value.points {
+				sum = sum + value.points[i].value
+			}
+			avg := (sum / float32(len(value.points)))
+			w.Write([]byte(fmt.Sprintf("%s %v \n", key, avg)))
+		}
+	}
+	for key, value := range h.metrics.counters {
+		var sum = float32(0)
+		if len(value.points) > 0 {
+			for i := range value.points {
+				sum = sum + value.points[i].value
+			}
+			w.Write([]byte(fmt.Sprintf("%s %v \n", key, sum)))
+		}
 	}
 }
