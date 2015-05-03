06d755cb2f9c7c67779229ac3e4fafbe0f08dcc4
arussellsaw <alexrussellsaw@gmail.com>
2015-05-03 20:26:30 +0100

    always report initialized metrics

--- httpMetrics.go
+++ httpMetrics.go
@@ -15,13 +15,14 @@ func (h httpMetrics) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	for key, value := range h.metrics.averages {
 		var sum = float32(0)
+		var avg = float32(0)
 		if len(value.points) > 0 {
 			for i := range value.points {
 				sum = sum + value.points[i].value
 			}
-			avg := (sum / float32(len(value.points)))
-			w.Write([]byte(fmt.Sprintf("%s %v \n", key, avg)))
+			avg = (sum / float32(len(value.points)))
 		}
+		w.Write([]byte(fmt.Sprintf("%s %v \n", key, avg)))
 	}
 	for key, value := range h.metrics.counters {
 		var sum = float32(0)
@@ -29,7 +30,7 @@ func (h httpMetrics) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			for i := range value.points {
 				sum = sum + value.points[i].value
 			}
-			w.Write([]byte(fmt.Sprintf("%s %v \n", key, sum)))
 		}
+		w.Write([]byte(fmt.Sprintf("%s %v \n", key, sum)))
 	}
 }
