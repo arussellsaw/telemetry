2ab168c08644c61c168c04b77325ad4392607acc
arussellsaw <alexrussellsaw@gmail.com>
2015-06-06 01:36:41 +0100

    change most everything

--- telemetry.go
+++ telemetry.go
@@ -1,92 +1,47 @@
 package telemetry
 
 import (
-	"net/http"
 	"time"
 )
 
-//Telemetry - Parent struct for all metrics
+//Telemetry container struct
 type Telemetry struct {
-	Counter *Counter
-	Average *Average
-	Total   *Total
-	Current *Current
-}
-
-//MetricInterface - Any type of metric
-type MetricInterface interface {
-	New(string, time.Duration) error
-	Add(string, float32)
-	Get(string) string
-	GetAll() map[string]float32
-}
-
-type metric struct {
-	points   []point
+	registry map[string]metricInterface
 	duration time.Duration
 }
 
-type point struct {
-	value     float32
-	timestamp time.Time
+type metricInterface interface {
+	GetName() string
+	Add(*Telemetry, float64) error
+	Get(*Telemetry) float64
+	Maintain()
 }
 
-//New - Initialize Telemetry, return Telemetry struct, and HTTPMetrics handler
-func New(cullSchedule time.Duration) (*Telemetry, http.Handler) {
-	var constructed Telemetry
-
-	counter := &Counter{
-		metric: make(map[string]metric),
+//New create new telemetry struct
+func New(duration time.Duration) *Telemetry {
+	tel := Telemetry{
+		registry: make(map[string]metricInterface),
+		duration: duration,
 	}
-	constructed.Counter = counter
-
-	average := &Average{
-		metric: make(map[string]metric),
-	}
-	constructed.Average = average
-
-	total := &Total{
-		metric: make(map[string]float32),
-	}
-	constructed.Total = total
-
-	current := &Current{
-		metric: make(map[string]float32),
-	}
-	constructed.Current = current
-
-	handler := HTTPMetrics{
-		Metrics: map[string]MetricInterface{
-			"averages": average,
-			"counters": counter,
-			"totals":   total,
-			"currents": current,
-		},
-	}
-	go constructed.cullScheduler(cullSchedule)
-	return &constructed, &handler
+	go tel.maintainance()
+	return &tel
 }
 
-func (t *Telemetry) cullScheduler(schedule time.Duration) {
+//GetAll get all metrics registered to Telemetry
+func (t *Telemetry) GetAll() map[string]float64 {
+	metrics := make(map[string]float64)
+	for _, metric := range t.registry {
+		metrics[metric.GetName()] = metric.Get(t)
+	}
+	return metrics
+}
+
+//maintainance run management on existing metrics
+func (t *Telemetry) maintainance() {
 	for {
-		for key := range t.Average.metric {
-			points := cull(t.Average.metric[key].points, t.Average.metric[key].duration)
-			t.Average.metric[key] = metric{points, t.Average.metric[key].duration}
+		for _, metric := range t.registry {
+			metric.Maintain()
 		}
-		for key := range t.Counter.metric {
-			points := cull(t.Counter.metric[key].points, t.Counter.metric[key].duration)
-			t.Counter.metric[key] = metric{points, t.Counter.metric[key].duration}
-		}
-		time.Sleep(schedule)
+		time.Sleep(t.duration)
 	}
-}
-
-func cull(points []point, ttl time.Duration) []point {
-	var culled []point
-	for i := range points {
-		if time.Since(points[i].timestamp) < ttl {
-			culled = append(culled, points[i])
-		}
-	}
-	return culled
 }
--- telemetry_test.go
+++ telemetry_test.go
@@ -1,76 +1,36 @@
 package telemetry_test
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/arussellsaw/telemetry"
 	. "github.com/smartystreets/goconvey/convey"
-	"github.com/stretchr/testify/assert"
 )
-
-func TestHTTP(t *testing.T) {
-	Convey("We should be able to fetch all metrics via the http handler", t, func() {
-		expected := `{
-  "averages": {},
-  "counters": {},
-  "currents": {},
-  "totals": {}
-}`
-		handler := telemetry.HTTPMetrics{
-			Metrics: map[string]telemetry.MetricInterface{
-				"averages": &telemetry.Average{},
-				"counters": &telemetry.Counter{},
-				"totals":   &telemetry.Total{},
-				"currents": &telemetry.Current{},
-			},
-		}
-		recorder := httptest.NewRecorder()
-		req, err := http.NewRequest("GET", "http://127.0.0.1/", nil)
-		assert.Nil(t, err)
-
-		handler.ServeHTTP(recorder, req)
-
-		So(recorder.Body.String(), ShouldEqual, expected)
-	})
-}
 
 //Counters
 
 func TestCounter(t *testing.T) {
 	Convey("Test Counters", t, func() {
 		Convey("New Counters should exist and be zero value", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Counter.New("test.counter", (60 * time.Second))
-			So(tel.Counter.Get("test.counter"), ShouldEqual, "test.counter 0")
+			tel := telemetry.New((100 * time.Millisecond))
+			counter := telemetry.NewCounter(tel, "test.counter", (60 * time.Second))
+			So(counter.Get(tel), ShouldEqual, 0)
 		})
 		Convey("Counters should equal the total of values added", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Counter.New("test.counter", (60 * time.Second))
-			tel.Counter.Add("test.counter", 10)
-			So(tel.Counter.Get("test.counter"), ShouldEqual, "test.counter 10")
+			tel := telemetry.New((100 * time.Millisecond))
+			counter := telemetry.NewCounter(tel, "test.counter", (60 * time.Second))
+			counter.Add(tel, float64(3))
+			So(counter.Get(tel), ShouldEqual, 3)
 		})
 		Convey("Values in counters should expire after their period has elapsed", func() {
-			tel, _ := telemetry.New((1 * time.Second))
-			tel.Counter.New("test.counter", (2 * time.Second))
-			tel.Counter.Add("test.counter", 10)
-			So(tel.Counter.Get("test.counter"), ShouldEqual, "test.counter 10")
-			time.Sleep(3 * time.Second)
-			So(tel.Counter.Get("test.counter"), ShouldEqual, "test.counter 0")
-		})
-		Convey("GetAll should return current values for all counters in a map", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Counter.New("test.counter", (60 * time.Second))
-			tel.Counter.New("test.counter2", (60 * time.Second))
-			tel.Counter.Add("test.counter", 10)
-			tel.Counter.Add("test.counter2", 20)
-			var expected = map[string]float32{
-				"test.counter":  10,
-				"test.counter2": 20,
-			}
-			assert.Equal(t, expected, tel.Counter.GetAll())
+			tel := telemetry.New((100 * time.Millisecond))
+			counter := telemetry.NewCounter(tel, "test.counter", (200 * time.Millisecond))
+			counter.Add(tel, float64(3))
+			So(counter.Get(tel), ShouldEqual, 3)
+			time.Sleep((300 * time.Millisecond))
+			counter.Maintain()
+			So(counter.Get(tel), ShouldEqual, 0)
 		})
 	})
 }
@@ -78,106 +38,28 @@ func TestCounter(t *testing.T) {
 //Averages
 
 func TestAverage(t *testing.T) {
-	Convey("Test Average metrics", t, func() {
-		Convey("New Averages should exist and be zero", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Average.New("test.average", (60 * time.Second))
-			So(tel.Average.Get("test.average"), ShouldEqual, "test.average 0")
+	Convey("Test Averages", t, func() {
+		Convey("New Averagess should exist and be zero value", func() {
+			tel := telemetry.New((100 * time.Millisecond))
+			avg := telemetry.NewAverage(tel, "test.avg", (60 * time.Second))
+			avg.Maintain()
+			So(avg.Get(tel), ShouldEqual, 0)
 		})
-		Convey("Averages should return the mean of all values within the time period", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Average.New("test.average", (60 * time.Second))
-			tel.Average.Add("test.average", 10)
-			tel.Average.Add("test.average", 20)
-			tel.Average.Add("test.average", 30)
-			So(tel.Average.Get("test.average"), ShouldEqual, "test.average 20")
+		Convey("Averages should equal the total of values added", func() {
+			tel := telemetry.New((100 * time.Millisecond))
+			avg := telemetry.NewAverage(tel, "test.avg", (60 * time.Second))
+			avg.Add(tel, float64(3))
+			avg.Maintain()
+			So(avg.Get(tel), ShouldEqual, 3)
 		})
-		Convey("GetAll should return means for all unique average keys", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Average.New("test.average", (60 * time.Second))
-			tel.Average.Add("test.average", 10)
-			tel.Average.Add("test.average", 20)
-
-			tel.Average.New("test.average2", (60 * time.Second))
-			tel.Average.Add("test.average2", 20)
-			tel.Average.Add("test.average2", 30)
-
-			var expected = map[string]float32{
-				"test.average":  15,
-				"test.average2": 25,
-			}
-			assert.Equal(t, expected, tel.Average.GetAll())
-		})
-	})
-}
-
-//Totals
-
-func TestTotal(t *testing.T) {
-	Convey("Test Total metrics", t, func() {
-		Convey("New Totals should exist and be zero", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Total.New("test.total", (60 * time.Second))
-			So(tel.Total.Get("test.total"), ShouldEqual, "test.total 0")
-		})
-		Convey("Totals should return the total of all values added in the applications lifetime", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Total.New("test.total", (60 * time.Second))
-			tel.Total.Add("test.total", 10)
-			tel.Total.Add("test.total", 20)
-			tel.Total.Add("test.total", 30)
-			So(tel.Total.Get("test.total"), ShouldEqual, "test.total 60")
-		})
-		Convey("GetAll should return totals of all unique total keys", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Total.New("test.total", (60 * time.Second))
-			tel.Total.Add("test.total", 10)
-			tel.Total.Add("test.total", 20)
-
-			tel.Total.New("test.total2", (60 * time.Second))
-			tel.Total.Add("test.total2", 20)
-			tel.Total.Add("test.total2", 30)
-
-			var expected = map[string]float32{
-				"test.total":  30,
-				"test.total2": 50,
-			}
-			assert.Equal(t, expected, tel.Total.GetAll())
-		})
-	})
-}
-
-//Currents
-
-func TestCurrent(t *testing.T) {
-	Convey("Test Current metrics", t, func() {
-		Convey("New Currents should exist and be zero", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Current.New("test.current", (60 * time.Second))
-			So(tel.Current.Get("test.current"), ShouldEqual, "test.current 0")
-		})
-		Convey("Currents should return the most recent value added to them", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Current.New("test.current", (60 * time.Second))
-			tel.Current.Add("test.current", 10)
-			tel.Current.Add("test.current", 20)
-			So(tel.Current.Get("test.current"), ShouldEqual, "test.current 20")
-		})
-		Convey("GetAll should return most recently added value for all unique current keys", func() {
-			tel, _ := telemetry.New((5 * time.Second))
-			tel.Current.New("test.current", (60 * time.Second))
-			tel.Current.Add("test.current", 10)
-			tel.Current.Add("test.current", 20)
-
-			tel.Current.New("test.current2", (60 * time.Second))
-			tel.Current.Add("test.current2", 20)
-			tel.Current.Add("test.current2", 10)
-
-			var expected = map[string]float32{
-				"test.current":  20,
-				"test.current2": 10,
-			}
-			assert.Equal(t, expected, tel.Current.GetAll())
+		Convey("Values in averages should expire after their period has elapsed", func() {
+			tel := telemetry.New((100 * time.Millisecond))
+			avg := telemetry.NewAverage(tel, "test.avg", (200 * time.Millisecond))
+			avg.Add(tel, float64(3))
+			avg.Maintain()
+			So(avg.Get(tel), ShouldEqual, 3)
+			time.Sleep((300 * time.Millisecond))
+			So(avg.Get(tel), ShouldEqual, 0)
 		})
 	})
 }
